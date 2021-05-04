a0f0cd897bcccd471ee55bdb08a3359bc45d1e41
elciomarques <elcio@elcio.info>
2021-05-03 22:53:11 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 //import NavBar from "./components/NavBar";
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-primary'>Olá mundo!</h1>
+    <>
       <NavBar />
-    </div>
+      <div className="container">
+        <h1 className='text-primary'>Olá mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
